0e1f73d9391e51d3edccd1662bbd077c87132543
ahavamorse <ahavamorse@icloud.com>
2021-04-26 14:21:32 -0700

    New attempt at eating cookies is successful, though slow for large amounts of cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -12,21 +12,20 @@ def eating_cookies(n, cache=None):
     if n == 2:
         return 2
     if n == 3:
-        return
+        return 4
     else:
-        return eating_cookies(n - 1) * n // 2
+        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        num_cookies = int(sys.argv[1])
-        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
-                                                                                    n=num_cookies))
-    else:
-        print('Usage: eating_cookies.py [num_cookies]')
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
+#                                                                                     n=num_cookies))
+#     else:
+#         print('Usage: eating_cookies.py [num_cookies]')
 
 import unittest
-from eating_cookies import eating_cookies
 
 class Test(unittest.TestCase):
 
@@ -44,4 +43,4 @@ class Test(unittest.TestCase):
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
