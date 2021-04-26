c4929ce03221c98f7870c0f074f12f4b596473be
ahavamorse <ahavamorse@icloud.com>
2021-04-26 14:13:30 -0700

    First attempt at rock paper scissor

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,33 @@
 
 import sys
 
+
+def add_possibilities(list_of_possibilities):
+    new_list = []
+    for possibility in list_of_possibilities:
+        possibility.append("rock")
+        new_list.append(possibility)
+        possibility.pop()
+        possibility.append("paper")
+        new_list.append(possibility)
+        possibility.pop()
+        possibility.append("scissors")
+        new_list.append(possibility)
+    return new_list
+
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    elif n == 1:
+        return [["rock"], ["paper"], ["scissors"]]
+    return add_possibilities(rock_paper_scissors(n - 1))
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
+
