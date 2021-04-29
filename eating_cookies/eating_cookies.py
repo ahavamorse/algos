6d012cdbadedaa81d0eccfdb7e56cfb8ff72dec3
ahavamorse <ahavamorse@icloud.com>
2021-04-29 15:51:12 -0700

    Fixed making cookies to actually use the cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,6 +7,8 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache={}):
+    if n in cache:
+        return cache[n]
     if n <= 1:
         cache[n] = 1
         return 1
@@ -22,10 +24,30 @@ def eating_cookies(n, cache={}):
         return possibilities
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        num_cookies = int(sys.argv[1])
-        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
-                                                                                    n=num_cookies))
-    else:
-        print('Usage: eating_cookies.py [num_cookies]')
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
+#                                                                                     n=num_cookies))
+#     else:
+#         print('Usage: eating_cookies.py [num_cookies]')
+
+import unittest
+
+class Test(unittest.TestCase):
+
+  def test_eating_cookies_small_n(self):
+    self.assertEqual(eating_cookies(0), 1)
+    self.assertEqual(eating_cookies(1), 1)
+    self.assertEqual(eating_cookies(2), 2)
+    self.assertEqual(eating_cookies(5), 13)
+    self.assertEqual(eating_cookies(10), 274)
+
+  def test_eating_cookies_large_n(self):
+    self.assertEqual(eating_cookies(50), 10562230626642)
+    self.assertEqual(eating_cookies(100), 180396380815100901214157639)
+    self.assertEqual(eating_cookies(500), 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527)
+
+
+if __name__ == '__main__':
+  unittest.main()
